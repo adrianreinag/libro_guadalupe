bc6ad0c30a0b47898886588eb252a74a14d01e55
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-10-05 11:06:19 +0200

    Eliminar PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import AstroPWA from "@vite-pwa/astro";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -11,17 +10,5 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
-    AstroPWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Libro del peregrino',
-        short_name: 'Libro',
-        // ...otras opciones del manifiesto
-      },
-      workbox: {
-        // Opciones de Workbox, como estrategias de caché
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'] // Incluye todos los tipos de archivos necesarios
-      },
-    }),
     solidJs()],
 });
