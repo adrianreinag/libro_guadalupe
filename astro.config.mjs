a8068b7fa029891ba547accea117c09b037354bb
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-10-06 00:31:08 +0200

    PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,63 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import AstroPWA from '@vite-pwa/astro'
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  style: {
-    postcss: {
-      plugins: [
-        // Aquí podrías añadir otros plugins de PostCSS si lo necesitas
-      ],
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+    server: {
+      fs: {
+        // Allow serving files from hoisted root node_modules
+        allow: ['../..']
+      }
     },
   },
-});
+  integrations: [
+    tailwind(),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      }
+    }),
+  ],
+})
