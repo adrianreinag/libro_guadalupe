984f49e371cd26f8056b4854fdc014d362de930c
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-10-05 11:25:33 +0200

    Eliminar react y solidsj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    react(),
-    solidJs()],
+    ],
 });
