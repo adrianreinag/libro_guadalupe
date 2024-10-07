c495bb0c6025476b4c5f7264cd518cd2081a0514
Adrián Reina Gálvez <adrianreinagalvez@gmail.com>
2024-10-07 11:34:24 +0200

    Actualizar estilos y funcionalidad de la página del Leccionario

--- src/classes/mayores.ts
+++ src/classes/mayores.ts
@@ -46,12 +46,12 @@ export class Laudes extends Mayores {
     titulo: "Salmo 94: Invitación a la alabanza divina",
     antifona: "",
     estrofas: [
-      "Venid, aclamemos al Señor,\ndemos vítores a la Roca que nos salva;\nentremos a su presencia dándole gracias,\naclamándolo con cantos.",
-      "Porque el Señor es un Dios grande,\nsoberano de todos los dioses:\ntiene en su mano las simas de la tierra,\nson suyas las cumbres de los montes;\nsuyo es el mar, porque él lo hizo,\nla tierra firme que modelaron sus manos.",
-      "Entrad, postrémonos por tierra,\nbendiciendo al Señor, creador nuestro.\nPorque él es nuestro Dios,\ny nosotros su pueblo,\nel rebaño que él guía.",
-      "Ojalá escuchéis hoy su voz:\n«No endurezcáis el corazón como en Meribá,\ncomo el día de Masá en el desierto;\ncuando vuestros padres me pusieron a prueba\ny me tentaron, aunque habían visto mis obras.",
-      'Durante cuarenta años\naquella generación me asqueó, y dije:\n"Es un pueblo de corazón extraviado,\nque no reconoce mi camino;\npor eso he jurado en mi cólera\nque no entrarán en mi descanso."',
-      "Gloria al Padre, y al Hijo, y al Espíritu Santo.\nComo era en el principio, ahora y siempre,\npor los siglos de los siglos. Amén.",
+      "Venid, aclamemos al Señor,<br>demos vítores a la Roca que nos salva;<br>entremos a su presencia dándole gracias,<br>aclamándolo con cantos.",
+      "Porque el Señor es un Dios grande,<br>soberano de todos los dioses:<br>tiene en su mano las simas de la tierra,<br>son suyas las cumbres de los montes;<br>suyo es el mar, porque él lo hizo,<br>la tierra firme que modelaron sus manos.",
+      "Entrad, postrémonos por tierra,<br>bendiciendo al Señor, creador nuestro.<br>Porque él es nuestro Dios,<br>y nosotros su pueblo,<br>el rebaño que él guía.",
+      "Ojalá escuchéis hoy su voz:<br>«No endurezcáis el corazón como en Meribá,<br>como el día de Masá en el desierto;<br>cuando vuestros padres me pusieron a prueba<br>y me tentaron, aunque habían visto mis obras.",
+      'Durante cuarenta años<br>aquella generación me asqueó, y dije:<br>"Es un pueblo de corazón extraviado,<br>que no reconoce mi camino;<br>por eso he jurado en mi cólera<br>que no entrarán en mi descanso."',
+      "Gloria al Padre, y al Hijo, y al Espíritu Santo.<br>Como era en el principio, ahora y siempre,<br>por los siglos de los siglos. Amén.",
     ],
     comentario: "Se repite la antífona.",
   };
@@ -59,12 +59,12 @@ export class Laudes extends Mayores {
     comentario: "Se hace la señal de la cruz mientras se comienza a recitar:",
     antifona: "",
     estrofas: [
-      "Bendito sea el Señor, Dios de Israel,\nporque ha visitado \ny redimido a su pueblo,\nsuscitándonos una fuerza \nde salvación\nen la casa de David, su siervo,\nsegún lo había predicho\ndesde antiguo\npor boca de sus santos Profetas.",
-      "Es la salvación que nos libra \nde nuestros enemigos\ny de la mano de todos \nlos que nos odian;\nrealizando la misericordia\nque tuvo con nuestros padres,\nrecordando su santa alianza\ny el juramento que juró \na nuestro padre Abrahán.",
-      "Para concedernos que,\nlibres de temor,\narrancados de la mano \nde los enemigos,\nle sirvamos con santidad y justicia,\nen su presencia, todos nuestros días.",
-      "Y a ti, niño, \nte llamarán profeta del Altísimo,\nporque irás delante del Señor\na preparar sus caminos,\nanunciando a su pueblo la salvación,\nel perdón de sus pecados.",
-      "Por la entrañable misericordia \nde nuestro Dios,\nnos visitará el sol \nque nace de lo alto,\npara iluminar \na los que viven en tinieblas\ny en sombra de muerte,\npara guiar nuestros pasos\npor el camino de la paz.",
-      "Gloria al Padre, y al Hijo, \ny al Espíritu Santo.\nComo era en el principio, \nahora y siempre,\npor los siglos de los siglos. \nAmén.",
+      "Bendito sea el Señor, Dios de Israel,<br>porque ha visitado <br>y redimido a su pueblo,<br>suscitándonos una fuerza <br>de salvación<br>en la casa de David, su siervo,<br>según lo había predicho<br>desde antiguo<br>por boca de sus santos Profetas.",
+      "Es la salvación que nos libra <br>de nuestros enemigos<br>y de la mano de todos <br>los que nos odian;<br>realizando la misericordia<br>que tuvo con nuestros padres,<br>recordando su santa alianza<br>y el juramento que juró <br>a nuestro padre Abrahán.",
+      "Para concedernos que,<br>libres de temor,<br>arrancados de la mano <br>de los enemigos,<br>le sirvamos con santidad y justicia,<br>en su presencia, todos nuestros días.",
+      "Y a ti, niño, <br>te llamarán profeta del Altísimo,<br>porque irás delante del Señor<br>a preparar sus caminos,<br>anunciando a su pueblo la salvación,<br>el perdón de sus pecados.",
+      "Por la entrañable misericordia <br>de nuestro Dios,<br>nos visitará el sol <br>que nace de lo alto,<br>para iluminar <br>a los que viven en tinieblas<br>y en sombra de muerte,<br>para guiar nuestros pasos<br>por el camino de la paz.",
+      "Gloria al Padre, y al Hijo, <br>y al Espíritu Santo.<br>Como era en el principio, <br>ahora y siempre,<br>por los siglos de los siglos. <br>Amén.",
     ],
   };
 
@@ -138,12 +138,12 @@ export class Visperas extends Mayores {
     titulo: "Salmo 94: Invitación a la alabanza divina",
     antifona: "",
     estrofas: [
-      "Venid, aclamemos al Señor,\ndemos vítores a la Roca que nos salva;\nentremos a su presencia dándole gracias,\naclamándolo con cantos.",
-      "Porque el Señor es un Dios grande,\nsoberano de todos los dioses:\ntiene en su mano las simas de la tierra,\nson suyas las cumbres de los montes;\nsuyo es el mar, porque él lo hizo,\nla tierra firme que modelaron sus manos.",
-      "Entrad, postrémonos por tierra,\nbendiciendo al Señor, creador nuestro.\nPorque él es nuestro Dios,\ny nosotros su pueblo,\nel rebaño que él guía.",
-      "Ojalá escuchéis hoy su voz:\n«No endurezcáis el corazón como en Meribá,\ncomo el día de Masá en el desierto;\ncuando vuestros padres me pusieron a prueba\ny me tentaron, aunque habían visto mis obras.",
-      'Durante cuarenta años\naquella generación me asqueó, y dije:\n"Es un pueblo de corazón extraviado,\nque no reconoce mi camino;\npor eso he jurado en mi cólera\nque no entrarán en mi descanso."',
-      "Gloria al Padre, y al Hijo, y al Espíritu Santo.\nComo era en el principio, ahora y siempre,\npor los siglos de los siglos. Amén.",
+      "Venid, aclamemos al Señor,<br>demos vítores a la Roca que nos salva;<br>entremos a su presencia dándole gracias,<br>aclamándolo con cantos.",
+      "Porque el Señor es un Dios grande,<br>soberano de todos los dioses:<br>tiene en su mano las simas de la tierra,<br>son suyas las cumbres de los montes;<br>suyo es el mar, porque él lo hizo,<br>la tierra firme que modelaron sus manos.",
+      "Entrad, postrémonos por tierra,<br>bendiciendo al Señor, creador nuestro.<br>Porque él es nuestro Dios,<br>y nosotros su pueblo,<br>el rebaño que él guía.",
+      "Ojalá escuchéis hoy su voz:<br>«No endurezcáis el corazón como en Meribá,<br>como el día de Masá en el desierto;<br>cuando vuestros padres me pusieron a prueba<br>y me tentaron, aunque habían visto mis obras.",
+      'Durante cuarenta años<br>aquella generación me asqueó, y dije:<br>"Es un pueblo de corazón extraviado,<br>que no reconoce mi camino;<br>por eso he jurado en mi cólera<br>que no entrarán en mi descanso."',
+      "Gloria al Padre, y al Hijo, y al Espíritu Santo.<br>Como era en el principio, ahora y siempre,<br>por los siglos de los siglos. Amén.",
     ],
     comentario: "Se repite la antífona.",
   };
@@ -152,11 +152,11 @@ export class Visperas extends Mayores {
     comentario: "Se hace la señal de la cruz mientras se comienza a recitar:",
     antifona: "",
     estrofas: [
-      "Proclama mi alma\nla grandeza del Señor,\nse alegra mi espíritu en Dios,\nmi salvador;\nporque ha mirado la humillación\nde su esclava.",
-      "Desde ahora me felicitarán\ntodas las generaciones,\nporque el Poderoso ha hecho\nobras grandes por mí:\nsu nombre es santo,\ny su misericordia llega a sus fieles\nde generación en generación.",
-      "Él hace proezas con su brazo:\ndispersa a los soberbios de corazón,\nderriba del trono a los poderosos\ny enaltece a los humildes,\na los hambrientos los colma de bienes\ny a los ricos los despide vacíos.",
-      "Auxilia a Israel, su siervo,\nacordándose de la misericordia\n–como lo había prometido a nuestros padres–\nen favor de Abrahán\ny su descendencia por siempre.",
-      "Gloria al Padre, y al Hijo,\ny al Espíritu Santo. \nComo era en el principio, \nahora y siempre, \npor los siglos de los siglos. \nAmén.",
+      "Proclama mi alma<br>la grandeza del Señor,<br>se alegra mi espíritu en Dios,<br>mi salvador;<br>porque ha mirado la humillación<br>de su esclava.",
+      "Desde ahora me felicitarán<br>todas las generaciones,<br>porque el Poderoso ha hecho<br>obras grandes por mí:<br>su nombre es santo,<br>y su misericordia llega a sus fieles<br>de generación en generación.",
+      "Él hace proezas con su brazo:<br>dispersa a los soberbios de corazón,<br>derriba del trono a los poderosos<br>y enaltece a los humildes,<br>a los hambrientos los colma de bienes<br>y a los ricos los despide vacíos.",
+      "Auxilia a Israel, su siervo,<br>acordándose de la misericordia<br>–como lo había prometido a nuestros padres–<br>en favor de Abrahán<br>y su descendencia por siempre.",
+      "Gloria al Padre, y al Hijo,<br>y al Espíritu Santo. <br>Como era en el principio, <br>ahora y siempre, <br>por los siglos de los siglos. <br>Amén.",
     ],
   };
 
